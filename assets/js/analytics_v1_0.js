daee5a658537b30b703418f634f653a690ef8d1d
curverush-game <cloudflare136@gmail.com>
2026-01-28 18:27:46 +0500

    Update analytics_v1_0.js

--- assets/js/analytics_v1_0.js
+++ assets/js/analytics_v1_0.js
@@ -15,10 +15,10 @@ function loadGoogleAnalytics(id) {
 window.addEventListener("load", function() {
     if (navigator.webdriver) {
       console.log('Bot Browser');
-      loadGoogleAnalytics("G-1R03PFRM0V");
+      loadGoogleAnalytics("G-84VXG0X4BM");
 
     } else {
       console.log('Human Browser');
-      loadGoogleAnalytics("G-1R03PFRM0V");
+      loadGoogleAnalytics("G-84VXG0X4BM");
     }
 });
